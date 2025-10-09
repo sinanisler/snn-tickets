1985899c8954998cf80aee77076c7bb74211184f
sinanisler <sinan@sinanisler.com>
2025-10-09 16:06:12 +0300

    release

--- snn-tickets.php
+++ snn-tickets.php
@@ -5,7 +5,7 @@
     Version: 0.4
     Requires PHP: 8.1
     Author: sinanisler
-    Author URI: https://sinanisler.com/              
+    Author URI: https://sinanisler.com/               
 */
 
 if (!defined('ABSPATH')) exit;
