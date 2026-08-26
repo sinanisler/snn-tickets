9ce2bf1ade9476606391b74ec71147273d2eeea2
sinanisler <sinan@sinanisler.com>
2026-08-26 21:35:28 +0300

    release

--- snn-tickets.php
+++ snn-tickets.php
@@ -4,7 +4,7 @@
     Description: Event tickets with server-side QR codes: a visual registration form builder, automatic or rule-based approval, confirmation emails, and a queued mailer that sends without keeping a browser tab open.
     Version: 0.21
     Requires PHP: 8.1
-    Author: sinanisler
+    Author: sinanisler 
     Author URI: https://sinanisler.com/ 
 */
 
